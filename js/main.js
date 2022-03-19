c97f0e965681794013d7008177904b1d6a5f7c9d
muhammetkaymak <kaymakmuhammet52@gmail.com>
2022-03-19 18:41:22 +0300

    header responsive yapısı oluşturuldu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.Header-switch').on('click', function () {
+  console.log('d')
+  if ($('Header').is('.menu-show')) {
+    $('.Header').removeClass('menu-show')
+  } else {
+    $('.Header').addClass('menu-show')
+  }
+})
