d349e9f11f3d77571dbe808ce54f7be1a903bac1
muhammetkaymak <kaymakmuhammet52@gmail.com>
2022-03-19 16:26:27 +0300

    header oluşturuldu

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const { notify } = require('browser-sync');
+const {src,dest,watch,series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const prefix = require('gulp-autoprefixer');
+const browsersync = require('browser-sync').create();
+
+function scssTask(){
+    return src('./scss/main.scss')
+    .pipe(sass())
+    .pipe(prefix())
+    .pipe(dest('./css/'))
+}
+
+function browsersyncServe(cb){
+    browsersync.init({
+        notify:false,
+        server: {
+            baseDir: './'
+        }
+    })
+    cb();
+}
+
+function browsersyncReload(cb){
+    browsersync.reload();
+    cb();
+}
+
+function watchTask(){
+    watch('*.html',browsersyncReload);
+    watch('./scss/**/*.scss',series(scssTask,browsersyncReload))
+}
+
+exports.default= series(
+    scssTask,
+    browsersyncServe,
+    watchTask
+);
